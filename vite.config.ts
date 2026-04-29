dceba1460003280cac46c930bc8aa1542fdd7d57
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-04-29 21:42:52 +0300

    feat: gh-pages deploy added

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/RSSchool_2026Q2_Stage3__ReactClassComponents/',
 });
