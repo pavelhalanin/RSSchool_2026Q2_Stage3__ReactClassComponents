22d5b1fbdd7af7c72af4cf3a9a19761e2646e678
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-05-09 20:46:33 +0300

    feat: minimum thresholds added on vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,14 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      thresholds: {
+        statements: 80,
+        branches: 50,
+        functions: 50,
+        lines: 50,
+      },
+    },
   },
 });
