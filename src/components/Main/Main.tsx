3bcfdb57b9ec753b5a2149605d235935bd67ddbb
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-05-16 16:07:34 +0300

    feat: Main class component changed to Main function component

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,15 +1,5 @@
-import { Component } from 'react';
 import CardList from '../CardList/CardList';
-import type GlobalState from './GlobalState';
 
-class Main extends Component<Record<string, never>, GlobalState> {
-  constructor(props: Record<string, never>) {
-    super(props);
-  }
-
-  render() {
-    return <CardList />;
-  }
+export default function Main() {
+  return <CardList />;
 }
-
-export default Main;
