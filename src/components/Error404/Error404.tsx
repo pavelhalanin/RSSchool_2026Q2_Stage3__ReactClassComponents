d2f93316e988198cd7534b51b6f5fb605274045a
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-05-16 21:11:08 +0300

    feat: css added for center image on 404 page

--- src/components/Error404/Error404.tsx
+++ src/components/Error404/Error404.tsx
@@ -5,8 +5,10 @@ export default function Error404() {
   return (
     <div className="container">
       <section className="section">
-        <h1 className="h1">Error 404</h1>
-        <img className={styles.image} src={image404} alt="" />
+        <div className={styles.image_block}>
+          <h1 className="h1">Error 404</h1>
+          <img className={styles.image} src={image404} alt="" />
+        </div>
       </section>
     </div>
   );
