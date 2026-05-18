de93b7d1fce609c2859e8c6ce01587f7dab14a53
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-05-18 20:07:27 +0300

    feat: removed feat that close card when click outside card

--- src/components/CardList/Right/CardListRight.tsx
+++ src/components/CardList/Right/CardListRight.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import type { IPokemon } from './IPokemon';
 import styles from '../CardList.module.css';
 import sleep from '../../../utils/sleep';
@@ -17,22 +17,6 @@ interface ICardData {
 }
 
 export function CardListRight(props: ICardListRight) {
-  const divRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    function handleClickOutside(event: MouseEvent) {
-      if (divRef.current && !divRef.current.contains(event.target as Node)) {
-        props.setParams(props.page, '');
-      }
-    }
-
-    document.addEventListener('mousedown', handleClickOutside);
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, []);
-
   const [cardData, setCardData] = useState<ICardData>({
     item: null,
     isFetch: false,
@@ -159,7 +143,7 @@ export function CardListRight(props: ICardListRight) {
   }, [props.details]);
 
   return (
-    <div ref={divRef}>
+    <>
       <div className={styles.card_list__right_block_buttons}>
         <button className="btn btn-danger" onClick={() => closeRight()}>
           Close
@@ -169,7 +153,7 @@ export function CardListRight(props: ICardListRight) {
         </button>
       </div>
       <CardListRightContent cardData={cardData} loadCard={loadCard} />
-    </div>
+    </>
   );
 }
 
