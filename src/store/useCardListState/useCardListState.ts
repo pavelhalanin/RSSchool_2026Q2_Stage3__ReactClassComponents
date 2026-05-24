a2d4a7cceeb5b1fa5acbed6db8dbf61c6e2ef431
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-05-24 21:23:17 +0300

    feat: csv filename and content updated

--- src/store/useCardListState/useCardListState.ts
+++ src/store/useCardListState/useCardListState.ts
@@ -198,7 +198,9 @@ export const useCardListState = create<ICardListState>((set, get) => ({
   downloadCsvItems() {
     const CSV_ROWS_ARRAY: Array<string> = [];
     const SEPARATOR = '\t';
-    CSV_ROWS_ARRAY.push(['id', 'name', 'weight', 'height'].join(SEPARATOR));
+    CSV_ROWS_ARRAY.push(
+      ['#', 'id', 'name', 'weight', 'height', 'types'].join(SEPARATOR)
+    );
 
     const STATE = get();
     const SELECTED_CARDS = STATE.csvItems;
@@ -207,18 +209,25 @@ export const useCardListState = create<ICardListState>((set, get) => ({
       const POKEMON = SELECTED_CARDS[i];
       CSV_ROWS_ARRAY.push(
         [
+          `${i + 1}`,
           `${POKEMON.id}`,
           `${POKEMON.name}`,
           `${POKEMON.weight}`,
           `${POKEMON.height}`,
+          `${POKEMON.pokemontypes.map((e) => e.type.name).join(' ')}`,
         ].join(SEPARATOR)
       );
     }
 
     const CSV = CSV_ROWS_ARRAY.join('\n');
 
-    const DATE_PREFIX: string = new Date().toJSON().slice(0, 19);
-    const FILENAME: string = `${DATE_PREFIX}_pokemon_selected_cards.csv`;
+    const DATE_PREFIX: string = new Date()
+      .toJSON()
+      .slice(0, 19)
+      .replaceAll(':', '-')
+      .replace('T', '_');
+    const COUNT_CARDS = SELECTED_CARDS.length;
+    const FILENAME: string = `[${DATE_PREFIX}]__${COUNT_CARDS}_pokemon.csv`;
     downloadFile_byText(FILENAME, CSV);
   },
 }));
