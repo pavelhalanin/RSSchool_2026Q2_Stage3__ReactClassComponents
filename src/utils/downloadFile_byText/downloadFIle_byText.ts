c9a2dc60517a804b55f76b1e7a6057982c95832f
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-05-24 21:38:08 +0300

    feat: test added for download text file

--- src/utils/downloadFile_byText/downloadFIle_byText.ts
+++ src/utils/downloadFile_byText/downloadFIle_byText.ts
@@ -1,9 +1,9 @@
 export function downloadFile_byText(filename: string, text: string) {
-  const element = document.createElement('a');
+  const a = document.createElement('a');
   const file = new Blob([text], { type: 'text/plain' });
-  element.href = URL.createObjectURL(file);
-  element.download = filename;
-  document.body.appendChild(element);
-  element.click();
-  document.body.removeChild(element);
+  a.href = URL.createObjectURL(file);
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
 }
