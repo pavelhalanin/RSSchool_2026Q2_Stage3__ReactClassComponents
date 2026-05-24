88d53dcd3b7b27a6398f4a2eec135d6c115590c8
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-05-24 21:47:27 +0300

    feat: theme checkbox added on top page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 import type { JSX } from 'react';
 import ContainerSection from '../ContainerSection/ContainerSection';
+import ThemeCheckbox from '../ThemeCheckbox/ThemeCheckbox';
 
 export default function Header(): JSX.Element {
   return (
@@ -16,6 +17,9 @@ export default function Header(): JSX.Element {
           </li>
           <li>
             <Link to="/404">404</Link>
+          </li>
+          <li>
+            <ThemeCheckbox />
           </li>
         </ul>
       </nav>
