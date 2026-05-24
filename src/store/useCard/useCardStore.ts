afda05c0f4a88c9dc7ca6ffa9e2393c4e5d17112
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-05-24 19:06:16 +0300

    feat: devtools added for card store

--- src/store/useCard/useCardStore.ts
+++ src/store/useCard/useCardStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { devtools } from 'zustand/middleware';
 import type { ICardState, ICardStore, IPokemon } from './types';
 import sleep from '../../utils/sleep';
 
@@ -8,115 +9,125 @@ const defaultCard: ICardState = {
   item: null,
 };
 
-export const useCardStore = create<ICardStore>((set) => ({
-  ...defaultCard,
-  actions: {
-    closeCard() {
-      set(() => ({
-        item: null,
-        isFetch: false,
-        errorFetch: null,
-      }));
-    },
-    generateFetchError() {
-      set(() => ({
-        item: null,
-        isFetch: false,
-        errorFetch: 'Custom test error HTTP 400-500',
-      }));
-    },
-    async loadCard_byDetails(details) {
-      if (!details) {
-        return;
-      }
+export const useCardStore = create<ICardStore>()(
+  devtools(
+    (set) => ({
+      ...defaultCard,
+      actions: {
+        closeCard() {
+          set(() => defaultCard, false, 'card/closeCard');
+        },
+        generateFetchError() {
+          set(
+            () => ({
+              item: null,
+              isFetch: false,
+              errorFetch: 'Custom test error HTTP 400-500',
+            }),
+            false,
+            'card/generateFetchError'
+          );
+        },
+        async loadCard_byDetails(details) {
+          if (!details) return;
 
-      set(() => ({
-        item: null,
-        isFetch: true,
-        errorFetch: null,
-      }));
+          set(
+            () => ({
+              item: null,
+              isFetch: true,
+              errorFetch: null,
+            }),
+            false,
+            'card/loadCard/start'
+          );
 
-      await sleep(500);
+          await sleep(500);
 
-      const GRAPHQL = `
-        query MyQuery {
-          pokemon(where: {id: {_eq: ${details}}}) {
-            base_experience
-            height
-            id
-            is_default
-            name
-            pokemon_species_id
-            weight
-            pokemonabilities {
-              ability {
-                abilitynames(where: {language: {id: {_eq: 9}}}) {
-                  id
-                  name
+          const GRAPHQL = `
+            query MyQuery {
+              pokemon(where: {id: {_eq: ${details}}}) {
+                base_experience
+                height
+                id
+                is_default
+                name
+                pokemon_species_id
+                weight
+                pokemonabilities {
+                  ability {
+                    abilitynames(where: {language: {id: {_eq: 9}}}) {
+                      id
+                      name
+                    }
+                  }
+                }
+                pokemontypes {
+                  slot
+                  type {
+                    name
+                  }
+                }
+                pokemonsprites {
+                  sprites
+                }
+                pokemoncries {
+                  cries
                 }
               }
             }
-            pokemontypes {
-              slot
-              type {
-                name
-              }
+          `;
+
+          const URL_ = `https://graphql.pokeapi.co/v1beta2`;
+
+          try {
+            const RESPONSE = await fetch(URL_, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                query: GRAPHQL,
+              }),
+            });
+
+            const HTTP_STATUS = RESPONSE.status;
+
+            if (HTTP_STATUS !== 200) {
+              const TEXT = await RESPONSE.text();
+              const MESSAGE = `HTTP ${HTTP_STATUS}\n${TEXT}`;
+
+              set(() => ({
+                item: null,
+                isFetch: false,
+                errorFetch: MESSAGE,
+              }));
             }
-            pokemonsprites {
-              sprites
-            }
-            pokemoncries {
-              cries
-            }
+
+            const DATA = await RESPONSE.json();
+            const POKEMON: IPokemon = DATA.data.pokemon[0];
+            set(
+              () => ({
+                item: POKEMON,
+                isFetch: false,
+                errorFetch: null,
+              }),
+              false,
+              'card/loadCard/success'
+            );
+          } catch (exception) {
+            set(
+              () => ({
+                item: null,
+                isFetch: false,
+                errorFetch: String(exception),
+              }),
+              false,
+              'card/loadCard/error'
+            );
           }
-        }
-      `;
-
-      const URL_ = `https://graphql.pokeapi.co/v1beta2`;
-
-      try {
-        const RESPONSE = await fetch(URL_, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({
-            query: GRAPHQL,
-          }),
-        });
-
-        const HTTP_STATUS = RESPONSE.status;
-
-        if (HTTP_STATUS !== 200) {
-          const TEXT = await RESPONSE.text();
-          const MESSAGE = `HTTP ${HTTP_STATUS}\n${TEXT}`;
-
-          set(() => ({
-            item: null,
-            isFetch: false,
-            errorFetch: MESSAGE,
-          }));
-        }
-
-        const DATA = await RESPONSE.json();
-        const POKEMON: IPokemon = DATA.data.pokemon[0];
-        set(() => ({
-          item: POKEMON,
-          isFetch: false,
-          errorFetch: null,
-        }));
-      } catch (exception) {
-        set(() => ({
-          item: null,
-          isFetch: false,
-          errorFetch: `${exception}`,
-        }));
-      } finally {
-        set((state) => ({
-          ...state,
-          isFetch: false,
-        }));
-      }
-    },
-  },
-}));
+        },
+      },
+    }),
+    { name: 'CardStore' }
+  )
+);
