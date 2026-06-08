62421384ee720181ce6a3d2384442eafa5adfa9a
PavelHalanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-06-08 20:50:10 +0300

    feat: vitest config formated

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
     coverage: {
-      provider: 'v8',
-      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      provider: "v8",
+      include: ["src/**/*.{js,jsx,ts,tsx}"],
       exclude: [
-        'src/**/*.test.{js,jsx,ts,tsx}',
-        'src/**/*.spec.{js,jsx,ts,tsx}',
-        'src/index.{js,jsx,ts,tsx}',
-        'src/setupTests.{js,ts}',
-        'src/**/*.d.ts',
-        'src/test-utils/**',
+        "src/**/*.test.{js,jsx,ts,tsx}",
+        "src/**/*.spec.{js,jsx,ts,tsx}",
+        "src/index.{js,jsx,ts,tsx}",
+        "src/setupTests.{js,ts}",
+        "src/**/*.d.ts",
+        "src/test-utils/**",
       ],
       thresholds: {
         statements: 80,
